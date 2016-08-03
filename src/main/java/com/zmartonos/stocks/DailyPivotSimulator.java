788e4a20cc24959b2754389d54dd40ae2155167f
zootanka <zootanka@gmail.com>
2016-08-03 02:31:36 +0200

    added trade sim:

--- src/main/java/com/zmartonos/stocks/DailyPivotSimulator.java
+++ src/main/java/com/zmartonos/stocks/DailyPivotSimulator.java
@@ -1,5 +1,6 @@
 package com.zmartonos.stocks;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javatuples.Pair;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,7 +26,8 @@ public class DailyPivotSimulator {
         final List<DailyPrice> dailyPrices = DailyPriceCSVReader.readDailyPriceCSV(
                 DailyPivotSimulator.class.getClassLoader().getResource("prices/henkel.csv").getFile());
 
-        // create pair of two consecutive trading day pricees
+        LOGGER.info("Prices: "+dailyPrices.get(0).getClose()+" :: "+dailyPrices.get(dailyPrices.size()-1).getClose());
+        // create pair of two consecutive trading day prices
         final List<Pair<DailyPrice, DailyPrice>> twoDayPrices =
                 IntStream.
                         range(0, dailyPrices.size() -1).
@@ -41,6 +44,9 @@ public class DailyPivotSimulator {
         AtomicInteger rising = new AtomicInteger(0);
         AtomicInteger falling = new AtomicInteger(0);
 
+        AtomicDouble purchase = new AtomicDouble(0);
+        AtomicDouble profit = new AtomicDouble(0);
+
         dailyPivots.stream().parallel().forEach(pivot->{
             DailyPrice dailyPrice = pivot.getDayPrice();
             if (dailyPrice.getClose() > dailyPrice.getOpen()){
@@ -48,7 +54,16 @@ public class DailyPivotSimulator {
             } else {
                 falling.incrementAndGet();
             }
+
+            if (pivot.getS1() > dailyPrice.getLow() && purchase.get() == 0.0){
+                purchase.set(pivot.getS1());
+            }
+
+            if (dailyPrice.getHigh() > pivot.getR1() && purchase.get() != 0.0){
+                profit.addAndGet(pivot.getR1()-purchase.get());
+                purchase.set(0.0);
+            }
         });
-        LOGGER.info("Rising: "+rising+" falling: "+falling);
+        LOGGER.info("Rising: "+rising+" falling: "+falling+" Profit: "+profit);
     }
 }
